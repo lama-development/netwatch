ef19bf43b8a93eefee14762871fdd17348d56928
lama-development <davide.lamarca04@gmail.com>
2025-02-27 17:04:34 +0100

    Moved DB path

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -1,10 +1,14 @@
 # src/app/db/database.py
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./netwatch.db"
+db_folder = os.path.join("src", "data")
+os.makedirs(db_folder, exist_ok=True)
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(db_folder, 'netwatch.db')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
